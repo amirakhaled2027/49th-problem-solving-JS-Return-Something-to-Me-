f8af8828d9ad4da317e87a793bbd38c1c0a4d84d
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-07-16 00:06:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+//"something"
+//"something" + sentence
+
+
+function giveMeSomething(sentence) {
+    let something = "something ";
+    return  something + sentence;
+}
+console.log(giveMeSomething("is better than nothing"));
+console.log(giveMeSomething("Bob Jane"));
+console.log(giveMeSomething("something"));
